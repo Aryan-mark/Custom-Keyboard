fb1b89d2ed0652a60b2d46f3a9c3333cbd9b7cd0
Aryan Jaiswal <aryanjaiswal@bitcot.com>
2025-12-24 16:18:24 +0530

    Initial Commit

---
+++ Keyboard/KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardApp.swift
+//  Keyboard
+//
+//  Created by Aryan Jaiswal on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
